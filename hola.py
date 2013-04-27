bfc5ea901d8d495d9f70bd088da9ca7c003e638f
Eduardo Antonio Mercader Galdames <eamercade@titan.(none)>
2013-04-27 16:47:02 -0400

    agrego encabezado

--- hola.py
+++ hola.py
@@ -1,3 +1,11 @@
+# Set de problemas #1
+# Problema 1.
+# Lenguaje y Tecnicas de Programacion 
+# Profesor: Igor Caracci
+# Profesor(Ayudante): Andrez Caro
+# Universidad de Santiago de Chile
+# 27 de abril del 2013
+
 nombre = input("Cual es tu nombre?")
 print("Hola",nombre)
  
